d7b96f937865f8dbd4538217ac7179db205cd7d6
vadimoblov <vadimoblov@gmail.com>
2015-06-15 16:11:41 +0300

    Merge origin/master
    Conflicts:
    	README.md

--- simple-messenger.js
+++ simple-messenger.js
@@ -3,11 +3,16 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
-http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=UTF-8'
-    });
-    
-    res.end('Hello from simple-messenger.\n');
-    
-}).listen(9080, "");
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resource not found.');
+    response.end();
+}
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(
+        200,
+        {"content-type": mime.lookup(path.basename(filePath))}
+    );
+    response.end(fileContents);
+}
