d7f04767940c452204c207d5627ef9b140e7e57a
vadimoblov <vadimoblov@gmail.com>
2015-06-15 16:05:45 +0300

    Merge origin/master
    Conflicts:
    	README.md

--- simple-messenger.js
+++ simple-messenger.js
@@ -1,10 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var cache = {};
 http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain; charset=UTF-8'
